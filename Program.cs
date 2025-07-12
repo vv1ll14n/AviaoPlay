328d25f41069b72d120c7da37bfa9e2272ca9322
Willian Oliveira <willinnoliveira@gmail.com>
2025-07-11 21:16:20 -0300

    Att
    Atualização arquivo program.cs - inicializado as variaveis  e criando a dinamica do aviao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+enum GameState
+{
+    MainMenu,
+    Playing,
+    GameOver
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Jogo do aviaozinho";
+        // dimensões do jogo
+        int screenWidth = 40;
+        int screenHeight = 20;
+        Console.SetWindowSize(screenWidth, screenHeight);
+        Console.SetBufferSize(screenWidth, screenHeight);
+
+        // Variaveis do Aviao
+        double AviaoVertical = screenHeight / 2.0;
+        double VelocidadeVertical = 0;
+        const double Gravidade = 0.2;
+        const double Voar = -1.0;
+
+        // iniciando o jogo
+
+        GameState gameState = GameState.MainMenu;
+
+    }
+}
